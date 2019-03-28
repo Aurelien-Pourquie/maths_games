75d2ff6d92a73a51ad912c6b4d7c233bc68110af
Aurelien <aurelien.pourquie@gmail.com>
2019-03-28 16:58:13 +0100

    version non OOP - test 1

--- pi/pi/Form1.Designer.cs
+++ pi/pi/Form1.Designer.cs
@@ -28,23 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(232, 566);
+            this.label1.Location = new System.Drawing.Point(12, 568);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(345, 600);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +85,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
 
--- pi/pi/Form1.cs
+++ pi/pi/Form1.cs
@@ -18,6 +18,10 @@ namespace pi
         private HttpClient getPi= new HttpClient();
         private HttpResponseMessage response = new HttpResponseMessage();
         private string pi;
+        private int score = 3;
+        private Label l1 = new Label();
+        private Label l2 = new Label();
+
 
         public Form1()
         {
@@ -28,9 +32,31 @@ namespace pi
         {
             new Thread(()=> { GetPi("https://uploadbeta.com/api/pi/?cached&n=5000"); }).Start();
             Thread.Sleep(1000);
-            affichePi();
+            affichePi(); // à supprimer
+            generateLabels();
+            
         }
 
+        private void generateLabels()
+        {
+            l1.Left = 13;
+            l1.Top = 100;
+            l1.Width = 320;
+            l1.Visible = true;
+            l1.ForeColor = Color.Black;
+            l1.Text = "pi = 3,";
+            Controls.Add(l1);
+
+            l2.Left = 13;
+            l2.Top = 125;
+            l2.Width = 320;
+            l2.Visible = true;
+            l2.ForeColor = Color.Black;
+            l2.Text = "";
+            Controls.Add(l2);
+        }
+
+        // test Api -- à supprimer
         private void affichePi()
         {
             label1.Text=pi;
@@ -45,5 +71,24 @@ namespace pi
             }            
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == pi[score].ToString()) {
+                if (score < 44) { l1.Text += textBox1.Text; }
+                else if (score < 94) { l2.Text += textBox1.Text; }
+                
+                textBox1.Text = "";
+                score++;
+            }
+            else { gameOver(); }
+        }
+
+        private void gameOver()
+        {
+            BackColor = Color.Red;
+            button1.Visible = false;
+            textBox1.Visible = false;
+            MessageBox.Show("Perdu !");
+        }
     }
 }
